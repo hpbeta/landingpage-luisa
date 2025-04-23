bac74031f95c7e123a5ddd2f85120329575c8cf8
hpbeta <hpbeta16.l@gmail.com>
2025-04-23 20:18:05 -0300

    Removendo algumas sessões

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { GlobalStyles } from "./globalStyles/globalStyles";
 import { AboutMe } from "./pages/aboutMe";
 import { ApproachSection } from "./pages/approachSection";
 import { CardSymptoms } from "./pages/cardSymptoms";
-import { CoreValues } from "./pages/coreValues";
 import { FaqAccordion } from "./pages/faqAcordeon";
 import { Footer } from "./pages/footer";
 import { Header } from "./pages/header";
@@ -11,7 +10,6 @@ import HeaderSection from "./pages/headerSection";
 import { Hero } from "./pages/hero";
 import InstagramSection from "./pages/instagramSection";
 import TabsSection from "./pages/tabSection";
-import { TherapySteps } from "./pages/therapySteps";
 import { WhyTherapy } from "./pages/whytherapy";
 
 function App() {
@@ -21,12 +19,10 @@ function App() {
       <WhatsAppButton />
       <Header />
       <Hero />
-      <CoreValues />
       <CardSymptoms />
       <WhyTherapy />
       <AboutMe />
       <ApproachSection />
-      <TherapySteps />
       <TabsSection />
       <FaqAccordion />
       <HeaderSection />
